fbd61769917b521160cfbe9f2e6c4dc2f8ce3752
denizozen <deniz.ozen@icloud.com>
2025-08-05 19:30:32 +0300

    Cleaned up repo

--- Topics/BigInteger/Double factorial/src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.math.BigInteger;
-
-class DoubleFactorial {
-    public static BigInteger calcDoubleFactorial(int n) {
-        // type your java code here
-    }
-}
--- Topics/Singleton/Complete a class/src/Main.java
+++
@@ -1,3 +0,0 @@
-class Singleton {
-    // write your code here
-}
